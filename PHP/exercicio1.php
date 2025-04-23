aaee3a5edd460395fda72f29d4b0631de7d7534d
Guizeraam <71233613+Guizeraam@users.noreply.github.com>
2025-04-23 01:12:56 -0300

    teste

---
+++ PHP/exercicio1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="recursos/css/estilo.css">
+    <link rel="stylesheet" href="recursos/css/exercicioo.css">
+    <title>Exercício</title>
+</head>
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Guilherme Ferraz</h1>
+        <h2>Visualização do Exercício</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde"> Sem formatação</a>
+        <a href="index.php" class="vermelho"> Voltar</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo"> 
+          <!--  -->
+            <?php
+            include ("{$_GET['dir']}/{$_GET['file']}.php");
+            ?>
+          
+        </div>
+    </main>
+    <footer class="rodape">
+        Guilherme Ferraz © <?= date('Y');?>
+    </footer>
+</body>
+
+</html>
---
+++ PHP/index1.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="recursos/css/estilo.css">
+    <title>Curso PHP</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Índice de exerícios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo"> 
+            <nav class="modulos">
+                <nav class="modulo laranja1">
+                    <h3>Módulo 01 - Básico</h3>
+                    <ul>
+                        <li><a href="exercicio.php?dir=teste&file=teste">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </nav>
+                <nav class="modulo laranja2">
+                    <h3>Módulo 02 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </nav>
+                <nav class="modulo laranja3">
+                    <h3>Módulo 03 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </nav>
+                <nav class="modulo laranja4">
+                    <h3>Módulo 04 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </nav>
+                <nav class="modulo laranja5">
+                    <h3>Módulo 05 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </nav>
+                <nav class="modulo laranja6">
+                    <h3>Módulo 06 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </nav>
+                <nav class="modulo laranja7">
+                    <h3>Módulo 07 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </nav>
+                <nav class="modulo laranja8">
+                    <h3>Módulo 08 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </nav>
+                <nav class="modulo laranja9">
+                    <h3>Módulo 09 - Básico</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                    </ul>
+                </nav>
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+        Guilherme Ferraz © <?= date('Y');?>
+    </footer>
+</body>
+
+</html>
